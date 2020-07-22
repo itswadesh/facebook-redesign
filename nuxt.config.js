3797a211ceedef467e33fdbb1f0e3fee64021d01
Swadesh Behera <itswadesh@gmail.com>
2020-07-22 11:57:28 +0530

    webfont

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,23 @@
 export default {
-  mode: "universal",
+  mode: 'universal',
   head: {
-    title: process.env.npm_package_name || "",
+    title: process.env.npm_package_name || '',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || "",
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
       },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-  buildModules: ["@nuxtjs/tailwindcss"],
-  modules: ["@nuxtjs/axios", "@nuxtjs/pwa"],
-};
+  buildModules: ['@nuxtjs/tailwindcss', 'nuxt-webfontloader'],
+  modules: ['@nuxtjs/axios', '@nuxtjs/pwa'],
+  webfontloader: {
+    google: {
+      families: ['Lato:400,700'], //Loads Lato font with weights 400 and 700
+    },
+  },
+}
